5d76baf02f93355712980371f4afa7dc244a46c5
Sang Yum <Sang_Yum@partech.com>
2022-01-05 15:07:46 -0800

    add dependency to useEffect

--- src/members/index.js
+++ src/members/index.js
@@ -24,7 +24,7 @@ const MembersContainer = ({user}) => {
             .finally(() => {
                 // always executed
             });
-    }, []);
+    }, [user.accessToken]);
 
     return (
         <Members members={members}/>
