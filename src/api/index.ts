008f8d7ba86b70df0df672e35601a55d37e30369
Sang Yum <Sang_Yum@partech.com>
2022-01-16 20:49:35 -0800

    added trace call

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,33 +1,35 @@
-import axios from 'axios'
-import { MemberType, User } from '../types';
+import axios from "axios";
+import { MemberType, User } from "../types";
 
 const url = process.env.REACT_APP_DIRECTORY_SERVICE_URL as string;
 
-const searchMembers = async (user: User, searchTerm: string): Promise<MemberType[]> => {
-    return await callEndpoint(user, `${url}?query=${searchTerm}`);    
-}
+const searchMembers = async (
+  user: User,
+  searchTerm: string
+): Promise<MemberType[]> => {
+  return await callEndpoint(user, `${url}?query=${searchTerm}`);
+};
 
 const getMembers = async (user: User): Promise<MemberType[]> => {
-    return await callEndpoint(user, url);    
-}
+  return await callEndpoint(user, url);
+};
 
 const callEndpoint = async (user: User, url: string): Promise<MemberType[]> => {
-    const options = {
-        headers: {
-            Authorization: `Bearer ${user.accessToken}`
-        }
-    };
-    try {
-        const { data } = await axios.get(url, options);
-        return data;
-    } catch (error) {
-        console.log('error', error);
-        return [];
-    }
+  const options = {
+    headers: {
+      Authorization: `Bearer ${user.accessToken}`,
+    },
+  };
 
-}
+  console.trace("api::callEndpoint");
 
-export {
-    getMembers,
-    searchMembers
-}
+  try {
+    const { data } = await axios.get(url, options);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    return [];
+  }
+};
+
+export { getMembers, searchMembers };
