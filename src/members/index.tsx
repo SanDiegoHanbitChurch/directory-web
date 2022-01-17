923a346baeb8caf4d7af4a616ea2ea01044bed73
Sang Yum <Sang_Yum@partech.com>
2022-01-16 21:06:06 -0800

    added additional console.log for debugging

--- src/members/index.tsx
+++ src/members/index.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import Members from './members';
-import { getMembers } from '../api/index';
-import { MemberType, User } from '../types';
+import React, { useEffect, useState } from "react";
+import Members from "./members";
+import { getMembers } from "../api/index";
+import { MemberType, User } from "../types";
 
 type Props = {
-    user: User
-}
+  user: User;
+};
 
-const MembersContainer = ({user}: Props) => {
-    const [membersState, setMembers] = useState<MemberType[]>([]);
-  
-    useEffect(() => {
-       const fetchData = async (user: User) => {
-            const members =  await getMembers(user);
-            setMembers(members)
-        };
-        fetchData(user)
-    }, [user]);
+const MembersContainer = ({ user }: Props) => {
+  const [membersState, setMembers] = useState<MemberType[]>([]);
+  console.log("membersState", membersState);
 
-    return (
-        <Members members={membersState}/>
-    )
-}
+  useEffect(() => {
+    const fetchData = async (user: User) => {
+      const members = await getMembers(user);
+      console.log("members", members);
+      setMembers(members);
+    };
+    fetchData(user);
+  }, [user]);
+
+  return <Members members={membersState} />;
+};
 
 export default MembersContainer;
